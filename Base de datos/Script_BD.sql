09a730c2f2bb33b614ee52c13dbf9255cb342593
Juana Paulina Aguila Hernandez <vegenisennawa@users.noreply.github.com>
2026-09-07 23:14:49 -0600

    Base de datos
    Generación de la tabla de login.

---
+++ Base de datos/Script_BD.sql
@@ -0,0 +1,25 @@
+/* 
+    Juana Paulina Águila Hernández
+    Script de prueba para Grupo San Carlos
+*/
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'GrupoSanCarlos_Test')
+BEGIN
+    CREATE DATABASE [GrupoSanCarlos_Test];
+END
+GO
+
+USE [GrupoSanCarlos_Test];
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Usuarios]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE Usuarios (
+        Id INT IDENTITY(1,1) PRIMARY KEY,
+        NombreUsuario VARCHAR(50) NOT NULL,
+        Password VARCHAR(255) NOT NULL
+    );
+END
+GO
+
+--Inserción de dato de prueba.
+INSERT INTO Usuarios (NombreUsuario, Password) VALUES ('admin', 'inmobiliaria2026');
